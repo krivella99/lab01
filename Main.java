f4d840250b0e5a4a118d0821794a5a9f4d636db8
krivella99 <92755064+krivella99@users.noreply.github.com>
2025-05-21 03:45:32 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import Controller.Controller;
-
-public class Main {
-    public static void main(String[] args) {
-        // Создаем контроллер и запускаем приложение
-        Controller controller = new Controller();
-        controller.startApp();
-    }
-}
